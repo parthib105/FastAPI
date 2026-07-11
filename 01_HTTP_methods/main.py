170420722c9e5a608a30cf303ac65c3bce8ba321
Projects <parthibg105@gmail.com>
2026-07-11 23:00:53 +0530

    Done with get method

---
+++ 01_HTTP_methods/main.py
@@ -0,0 +1,22 @@
+import json
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    
+    return data
+
+@app.get("/")
+def hello():
+    return {"message": "Patient Management System API"}
+
+@app.get("/about")
+def about():
+    return {"message": "A fully functional API to manage your patient records"}
+
+@app.get("/view")
+def view():
+    return load_data()
